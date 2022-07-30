46259e54f1a44a1638d825bb1c6260a9eaba4186
Rodrigo Serieiro <rodrigo.serieiro@outlook.com>
2022-07-30 18:34:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+function formatDate(date, format){
+    const map = {
+        mm: date.getMonth() + 1,
+        dd: date.getDate(),
+        aa: date.getFullYear().toString().slice(-2),
+        aaaa: date.getFullYear()
+    }
+
+    return format.replace(/mm|dd|aa|aaaa/gi, matched => map[matched])
+}
+showTime();
+formatDate(date, format);
